ff5f9ee4f64d9e3795223e1484c680a67c003ab2
B-U-B-A-86-RUS <155003986+B-U-B-A-86-RUS@users.noreply.github.com>
2024-01-17 15:36:15 +0500

    Initial commit

---
+++ jest/jest.config.js
@@ -0,0 +1,11 @@
+{
+  import("jest").Config;
+}
+const config = {
+  collectCoverageFrom: [
+    "**/*.{js,jsx}",
+    "!**/node_modules/**",
+    "!**/vendor/**",
+  ],
+};
+module.exports = config;
