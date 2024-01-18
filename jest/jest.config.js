83ceb22b532c1a199878f20d29ad5f7ed589118f
B-U-B-A-86-RUS <155003986+B-U-B-A-86-RUS@users.noreply.github.com>
2024-01-18 20:54:33 +0500

    Jest commit

--- jest/jest.config.js
+++ jest/jest.config.js
@@ -1,11 +1,13 @@
-{
-  import("jest").Config;
-}
 const config = {
   collectCoverageFrom: [
     "**/*.{js,jsx}",
     "!**/node_modules/**",
     "!**/vendor/**",
   ],
+  "coverageThreshold": {
+    "branches": 100,
+    "functions": 100,
+    "lines": 100
+  }
 };
-module.exports = config;
+
