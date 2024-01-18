ead6e82a3ce54c81026994092e5897a90456fc4a
B-U-B-A-86-RUS <155003986+B-U-B-A-86-RUS@users.noreply.github.com>
2024-01-18 22:33:47 +0500

    Playwright commit

---
+++ playwright/tests/test.spec.js
@@ -0,0 +1,17 @@
+const { test, expect, chromium } = require("@playwright/test");
+const user = require("../user");
+
+test("test", async ({page}))
+{
+    const browser = await chromium.launch({
+        headless: false,
+        slowMo: 5000
+    });
+ await page.goto("https://netology.ru/?modal=sign_in");
+ await page.locator('[placeholder="Email"]').click();
+ await page.locator('[placeholder="Email"]').fill(user.email);
+ await page.locator('[placeholder="Пароль"]').click();
+ await page.locator('[placeholder="Пароль"]').fill(user.password);
+ await page.locator('[data-testid="login-submit-btn"]').click();
+ await page.waitForUrl("https://netology.ru/profile/");
+}
